9616bbc03f3e54e95e64bb436d10ebed7761e660
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2016-05-27 18:44:24 +0200

    first commit

---
+++ coolamqp/README.md
@@ -0,0 +1,8 @@
+coolamqp
+========
+
+The one library to rule them all.
+
+This is a fault-tolerant-able AMQP library.
+
+Under the hood it uses pyamqp 
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='CoolAMQP',
+      version='1.0',
+      description='The AMQP client library',
+      author=u'Piotr Maślanka',
+      author_email='piotrm@smok.co',
+      url='https://github.com/piotrmaslanka/coolamqp',
+      packages=['coolamqp'],
+     )
