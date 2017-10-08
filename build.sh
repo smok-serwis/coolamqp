283df069d422e50f1bb08a221df0c264dff39b5c
Piotr Maslanka <piotr.maslanka@henrietta.com.pl>
2017-10-08 04:43:26 +0200

    dont install compiler

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -x
 set -e
 
 python -m coolamqp.framing.compilation.compile_definitions
+rm -f coolamqp/framing/compilation/compile_definitions.py
 python setup.py bdist bdist_wheel
 
 if [ $TRAVIS_BRANCH == "master" ]; then
