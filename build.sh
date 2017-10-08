43f76a188856635a061a0b321868fba042bd3915
Piotr Maslanka <piotr.maslanka@henrietta.com.pl>
2017-10-08 06:16:25 +0200

    upload only once

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ python compile_definitions.py
 python setup.py bdist bdist_wheel
 
 if [ $TRAVIS_BRANCH == "master" ]; then
-    pip install wheel twine
-    twine upload -u $PYPI_USER -p $PYPI_PWD dist/*
+
+    if [ $TRAVIS_PYTHON_VERSION == "2.7" ]; then
+        pip install wheel twine
+        twine upload -u $PYPI_USER -p $PYPI_PWD dist/*
+    fi
 fi
