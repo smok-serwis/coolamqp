0ef6d5d12b7ad5527464a43c65f424867b944881
Piotr Maslanka <piotr.maslanka@henrietta.com.pl>
2017-10-08 05:10:31 +0200

    compiling moved outside

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 set -x
 set -e
 
-python -m coolamqp.framing.compilation.compile_definitions
-rm -f coolamqp/framing/compilation/compile_definitions.py
+python compile_definitions.py
 python setup.py bdist bdist_wheel
 
 if [ $TRAVIS_BRANCH == "master" ]; then
--- tests/test_framing/test_compilation.py
+++ tests/test_framing/test_compilation.py
@@ -1,12 +1,12 @@
 # coding=UTF-8
 from __future__ import print_function, absolute_import, division
-import six
+
 import unittest
 
 
 class TestCompilation(unittest.TestCase):
     def test_comp(self):
-        from coolamqp.framing.compilation.compile_definitions import \
+        from compile_definitions import \
             compile_definitions
         compile_definitions(xml_file='resources/amqp0-9-1.extended.xml',
                             out_file='/tmp/definitions.py')
