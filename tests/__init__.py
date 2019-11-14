a1067978bbfe3f35ce246d0afedc7359a4438951
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2019-11-14 22:39:30 +0100

    gevent is not supported on PyPy

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,14 @@
 import os
 import logging
-
+import platform
+import sys
 logger = logging.getLogger(__name__)
 
 
 if 'ENABLE_GEVENT' in os.environ:
+    if platform == 'PyPy':
+        # gevent is not supported on PyPy
+        sys.exit(0)
     import gevent.monkey
     logger.warning('Monkey patching the environment with gevent!')
     gevent.monkey.patch_all()
