eb7b6ca0b65470808a3ef10c1d91cb497c1029ec
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-01-03 18:26:07 +0100

    exceptions

--- coolamqp/exceptions.py
+++ coolamqp/exceptions.py
@@ -1,7 +1,10 @@
 # coding=UTF-8
 from __future__ import absolute_import, division, print_function
 
-from coolamqp.framing.definitions import HARD_ERRORS
+from coolamqp.framing.definitions import HARD_ERRORS, RESOURCE_LOCKED
+
+
+__all__ ['HARD_ERRORS', 'RESOURCE_LOCKED', 'CoolAMQPError', 'ConnectionDead', 'AMQPError']
 
 
 class CoolAMQPError(Exception):
