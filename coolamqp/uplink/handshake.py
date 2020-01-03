a3e1417b4aed6c40bb4f8a12c3a99b283ae8f8ef
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-01-03 01:23:02 +0100

    stress test v0.98

--- coolamqp/uplink/handshake.py
+++ coolamqp/uplink/handshake.py
@@ -25,7 +25,7 @@ CLIENT_DATA = [
     # because RabbitMQ is some kind of a fascist and does not allow
     # these fields to be of type short-string
     (b'product', (b'CoolAMQP', 'S')),
-    (b'version', (b'0.97a1', 'S')),
+    (b'version', (b'0.98', 'S')),
     (b'copyright', (b'Copyright (C) 2016-2019 SMOK sp. z o.o.', 'S')),
     (
         b'information', (
