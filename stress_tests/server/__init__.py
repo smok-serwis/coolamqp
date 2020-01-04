fdf5ddaa0d7bbedd9cc673cd55c818ac45d7d923
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-01-04 20:21:30 +0100

    a minor change

--- stress_tests/server/__init__.py
+++ stress_tests/server/__init__.py
@@ -1,10 +1,10 @@
 import logging
 
+from satella.coding.concurrent import TerminableThread
+
 from coolamqp.clustering.events import ReceivedMessage
 from coolamqp.objects import Queue, Message
 
-logger = logging.getLogger(__name__)
-from satella.coding.concurrent import TerminableThread
 from ..settings import queue_names, connect, LogFramesToFile
 
 
@@ -20,8 +20,7 @@ class Server(TerminableThread):
     def loop(self):
         evt = self.amqp.drain(timeout=1.0)
         if isinstance(evt, ReceivedMessage):
-            routing_key = evt.routing_key.tobytes().decode('utf8')
-            routing_key = routing_key.replace('-repl', '')
+            routing_key = evt.routing_key.tobytes().decode('utf8').replace('-repl', '')
             self.amqp.publish(Message(evt.body), routing_key=routing_key)
 
 
