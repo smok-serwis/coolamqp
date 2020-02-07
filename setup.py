e56209aa591f2633c22ee65e612417d1eae73fc4
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-02-07 23:11:49 +0100

    setup docs fixed

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ from coolamqp import __version__
 setup(keywords=['amqp', 'rabbitmq', 'client', 'network', 'ha', 'high availability'],
       version=__version__,
       packages=find_packages(include=['coolamqp', 'coolamqp.*']),
-      long_description=u'''Pure Python AMQP client, but with dynamic class generation and memoryviews FOR THE GODSPEED.
-
-Also, handles your reconnects and transactionality THE RIGHT WAY, though somewhat opinionated''',
       install_requires=['six', 'monotonic', 'futures', 'typing'],
       # per coverage version for codeclimate-reporter
       tests_require=["nose2", "coverage", "nose2[coverage_plugin]"],
