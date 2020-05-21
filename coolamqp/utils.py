59fbf1a7609afb35117d1706b1bdd7741ac921e5
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-05-21 18:58:59 +0200

    ImportError fix

--- coolamqp/utils.py
+++ coolamqp/utils.py
@@ -1,11 +1,17 @@
+
+try:
+    IMPORT_ERRORS = (ModuleNotFoundError, ImportError)
+except NameError:
+    IMPORT_ERRORS = (ImportError, )
+
 try:
     from time import monotonic
-except ImportError:
+except IMPORT_ERRORS:
     from monotonic import monotonic
 
 try:
     from prctl import set_name as prctl_set_name
-except ImportError:
+except IMPORT_ERRORS:
     def prctl_set_name(name):
         pass
 
