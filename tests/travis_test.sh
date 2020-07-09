596a61d6e90aaf96e107f56f21f97b03c178341b
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-07-09 18:00:32 +0200

    append the coverage

--- tests/travis_test.sh
+++ tests/travis_test.sh
@@ -11,7 +11,7 @@ python setup.py install
 pip install -r stress_tests/requirements.txt
 pip install yapf nose2 mock coverage nose2[coverage_plugin]
 
-coverage run -m compile_definitions
+coverage run --append -m compile_definitions
 coverage run --append -m nose2 -vv
 COOLAMQP_FORCE_SELECT_LISTENER=1 coverage run --append -m nose2 -vv
 coverage run --append -m stress_tests
